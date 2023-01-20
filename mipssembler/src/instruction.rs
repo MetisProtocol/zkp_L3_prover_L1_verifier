2b4f69db4461fbb134d7267f0d434bdac56c5703
root <root@ip-172-31-44-129.us-east-2.compute.internal>
2023-01-20 17:49:24 +0000

    Added logics related to mips

---
+++ mipssembler/src/instruction.rs
@@ -0,0 +1,57 @@
+use crate::text::Text;
+
+pub enum InstructionFormat {
+    REGISTER,
+    IMMEDIATE,
+    JUMP,
+    PSEUDO,
+}
+
+pub struct Instruction {
+    pub name: &'static str,
+    pub opcode: i32,
+    pub funct: i32,
+}
+
+impl Instruction {
+    pub const fn new(name: &'static str, opcode: i32, funct: i32) -> Self {
+        Self {
+            name,
+            opcode,
+            funct,
+        }
+    }
+
+    pub fn is_branch(&self) -> bool {
+        self.opcode == 4 || self.opcode == 5
+    }
+
+    pub fn is_shift(&self) -> bool {
+        self.funct == 0 || self.funct == 2
+    }
+
+    pub fn is_register_jump(&self) -> bool {
+        self.funct == 8
+    }
+
+    pub fn to_register_format_text(&self, rs: i32, rt: i32, rd: i32, shamt: i32) -> Text {
+        Text::new(rs, rt, rd, shamt, self.funct, self.opcode, 0, 0)
+    }
+
+    pub fn to_jump_format_text(&self, address: i32) -> Text {
+        Text::new(0, 0, 0, 0, self.funct, self.opcode, 0, address)
+    }
+
+    pub fn to_immediate_format_text(&self, rs: i32, rt: i32, immediate: i32) -> Text {
+        Text::new(rs, rt, 0, 0, self.funct, self.opcode, immediate, 0)
+    }
+}
+
+pub fn convert_opcode_to_format(opcode: i32) -> InstructionFormat {
+    match opcode {
+        0 => InstructionFormat::REGISTER,
+        2 | 3 => InstructionFormat::JUMP,
+        -1 => InstructionFormat::PSEUDO,
+        _ => InstructionFormat::IMMEDIATE,
+    }
+}
---
+++ mipssembler/tests/main_test.rs
@@ -0,0 +1,113 @@
+use assert_cmd::prelude::*;
+use std::io::{Read, Write};
+use std::process::Command;
+use tempfile::NamedTempFile;
+
+mod fixtures;
+
+const BIN_NAME: &str = "mipssembler";
+
+#[test]
+fn test_main_case_1() {
+    use fixtures::{INPUT_CASE_1, OUTPUT_CASE_1};
+
+    let mut input_file = NamedTempFile::new().unwrap();
+    input_file.write_all(INPUT_CASE_1.as_bytes()).unwrap();
+
+    let mut output_file = NamedTempFile::new().unwrap();
+
+    Command::cargo_bin(BIN_NAME)
+        .unwrap()
+        .args(&[input_file.path(), output_file.path()])
+        .assert()
+        .success();
+
+    let mut actual = String::new();
+    output_file.read_to_string(&mut actual).unwrap();
+
+    assert_eq!(actual, OUTPUT_CASE_1);
+}
+
+#[test]
+fn test_main_case_2() {
+    use fixtures::{INPUT_CASE_2, OUTPUT_CASE_2};
+
+    let mut input_file = NamedTempFile::new().unwrap();
+    input_file.write_all(INPUT_CASE_2.as_bytes()).unwrap();
+
+    let mut output_file = NamedTempFile::new().unwrap();
+
+    Command::cargo_bin(BIN_NAME)
+        .unwrap()
+        .args(&[input_file.path(), output_file.path()])
+        .assert()
+        .success();
+
+    let mut actual = String::new();
+    output_file.read_to_string(&mut actual).unwrap();
+
+    assert_eq!(actual, OUTPUT_CASE_2);
+}
+
+#[test]
+fn test_main_case_3() {
+    use fixtures::{INPUT_CASE_3, OUTPUT_CASE_3};
+
+    let mut input_file = NamedTempFile::new().unwrap();
+    input_file.write_all(INPUT_CASE_3.as_bytes()).unwrap();
+
+    let mut output_file = NamedTempFile::new().unwrap();
+
+    Command::cargo_bin(BIN_NAME)
+        .unwrap()
+        .args(&[input_file.path(), output_file.path()])
+        .assert()
+        .success();
+
+    let mut actual = String::new();
+    output_file.read_to_string(&mut actual).unwrap();
+
+    assert_eq!(actual, OUTPUT_CASE_3);
+}
+
+#[test]
+fn test_main_case_4() {
+    use fixtures::{INPUT_CASE_4, OUTPUT_CASE_4};
+
+    let mut input_file = NamedTempFile::new().unwrap();
+    input_file.write_all(INPUT_CASE_4.as_bytes()).unwrap();
+
+    let mut output_file = NamedTempFile::new().unwrap();
+
+    Command::cargo_bin(BIN_NAME)
+        .unwrap()
+        .args(&[input_file.path(), output_file.path()])
+        .assert()
+        .success();
+
+    let mut actual = String::new();
+    output_file.read_to_string(&mut actual).unwrap();
+
+    assert_eq!(actual, OUTPUT_CASE_4);
+}
+
+#[test]
+fn test_main_case_5() {
+    use fixtures::{INPUT_CASE_5, OUTPUT_CASE_5};
+
+    let mut input_file = NamedTempFile::new().unwrap();
+    input_file.write_all(INPUT_CASE_5.as_bytes()).unwrap();
+
+    let mut output_file = NamedTempFile::new().unwrap();
+
+    Command::cargo_bin(BIN_NAME)
+        .unwrap()
+        .args(&[input_file.path(), output_file.path()])
+        .assert()
+        .success();
+
+    let mut actual = String::new();
+    output_file.read_to_string(&mut actual).unwrap();
+
+    assert_eq!(actual, OUTPUT_CASE_5);
+}
